cb5a697d084777e37da12d756e5a13e43ba9643c
alxkai <alex@instalily.ai>
2024-05-03 14:10:13 -0400

    Update CheckboxFacet.js

--- partselect-search/client/src/components/Facets/CheckboxFacet/CheckboxFacet.js
+++ partselect-search/client/src/components/Facets/CheckboxFacet/CheckboxFacet.js
@@ -1,5 +1,5 @@
 import React, { useContext, useState, useEffect } from 'react';
-import { Collapse, Checkbox, List, ListItem, ListItemText, Radio } from '@mui/material';
+import { Collapse, Checkbox, List, ListItem, ListItemText } from '@mui/material';
 import { ExpandLess, ExpandMore } from '@mui/icons-material';
 import styled from 'styled-components';
 
@@ -7,66 +7,50 @@ import './CheckboxFacet.css';
 import { AppContext } from '../../../contexts/AppContext';
 
 export default function CheckboxFacet(props) {
-    const { postSearchHandler, endOfModelList, seeMore, setModelNameDesc, setExactModelMatch, setModelNumSearch } = useContext(AppContext);
-
-    // Initialize state based on window width
+    const { postSearchHandler, setModelNameDesc, setModelNumSearch } = useContext(AppContext);
     const [isExpanded, setIsExpanded] = useState(window.innerWidth > 768);
 
     useEffect(() => {
-        function handleResize() {
+        const handleResize = () => {
             setIsExpanded(window.innerWidth > 768);
-        }
+        };
 
         window.addEventListener('resize', handleResize);
-
-        return () => {
-            window.removeEventListener('resize', handleResize);
-        };
+        return () => window.removeEventListener('resize', handleResize);
     }, []);
 
-    let [selectedModel, setSelectedModel] = useState(null);
-    const handleModelChange = (event, id, model) => {
-        if (selectedModel === model) {
-            setSelectedModel(null);
-            event.target.checked = false;
-        } else {
-            setSelectedModel(model);
-            setModelNameDesc(model);
-            setModelNumSearch(true);
-            postSearchHandler(id);
-        }
+    const handleFacetClick = (id, model) => {
+        setModelNameDesc(model);
+        setModelNumSearch(true);
+        postSearchHandler(id);
     };
 
-    const checkboxes = props.values.map(facetValue => {
-        let isSelected = props.selectedFacets.some(facet => facet.value === facetValue.value);
-
-        return (
+    const facetItems = props.values.map(facetValue => {
+        const isSelected = props.selectedFacets.some(facet => facet.value === facetValue.value);
+        
+        return props.name === "Model Number" ? (
+            <FacetValueListItem
+                dense
+                disableGutters
+                key={facetValue.value}
+                button
+                onClick={() => handleFacetClick(facetValue.id, facetValue.value)}
+            >
+                <ListItemText primary={facetValue.value} />
+            </FacetValueListItem>
+        ) : (
             <FacetValueListItem dense disableGutters id={facetValue.value} key={facetValue.value}>
-                {props.name !== "Model Number" ? (
-                    <>
-                        <Checkbox
-                            edge="start"
-                            disableRipple
-                            checked={isSelected}
-                            onChange={() => {
-                                isSelected 
-                                    ? props.removeFilter({ field: props.name, value: facetValue.value })
-                                    : props.addFilter(props.name, facetValue.value);
-                            }}
-                        />
-                        <ListItemText primary={`${facetValue.value} (${facetValue.count})`}/>
-                    </>
-                ) : (
-                    <>
-                        <Radio
-                            edge="start"
-                            disableRipple
-                            checked={selectedModel === facetValue.value}
-                            onChange={(event) => handleModelChange(event, facetValue.id, facetValue.value)}
-                        />
-                        <ListItemText primary={`${facetValue.value}`}/>
-                    </>
-                )}
+                <Checkbox
+                    edge="start"
+                    disableRipple
+                    checked={isSelected}
+                    onChange={() => {
+                        isSelected 
+                            ? props.removeFilter({ field: props.name, value: facetValue.value })
+                            : props.addFilter(props.name, facetValue.value);
+                    }}
+                />
+                <ListItemText primary={`${facetValue.value} (${facetValue.count})`} />
             </FacetValueListItem>
         );
     });
@@ -80,10 +64,9 @@ export default function CheckboxFacet(props) {
                 {isExpanded ? <ExpandLess /> : <ExpandMore />}
             </FacetListItem>
             <Collapse in={isExpanded} component="div">
-                <FacetValuesList>
-                    {checkboxes}
-                    {props.name === "Model Number" && !endOfModelList && <div className="see-more" role="button" tabIndex="0" onClick={seeMore}>See More...</div>}
-                </FacetValuesList>
+                <List component="div" disablePadding>
+                    {facetItems}
+                </List>
             </Collapse>
         </div>
     );
@@ -91,12 +74,12 @@ export default function CheckboxFacet(props) {
 
 const FacetListItem = styled(ListItem)({
     paddingLeft: '16px !important',
-})
+});
 
-const FacetValueListItem= styled(ListItem)({
+const FacetValueListItem = styled(ListItem)({
     paddingLeft: '32px !important',
 });
 
-const FacetValuesList= styled(List)({
+const FacetValuesList = styled(List)({
     marginRight: '18px !important'
 });
